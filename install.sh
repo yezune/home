22e7411033d2a49b2c9b50e6e465198233526581
Choi, Yezune <yezune@gmail.com>
2021-10-29 16:46:06 +0900

    create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim  && \
+curl -L https://raw.githubusercontent.com/yezune/home/main/.vimrc > .vimrc  &&\
+curl -L https://raw.githubusercontent.com/yezune/home/main/.tmux.conf > .tmux.conf
