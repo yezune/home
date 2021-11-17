5b340a4757c2f7c98baec1d6551bd27d81087a52
Choi, Yezune <yezune@gmail.com>
2021-11-17 11:41:09 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim  && \
-curl -L https://raw.githubusercontent.com/yezune/home/main/.vimrc > .vimrc  &&\
-curl -L https://raw.githubusercontent.com/yezune/home/main/.tmux.conf > .tmux.conf
+curl -L https://raw.githubusercontent.com/yezune/home/main/.vimrc > ~/.vimrc  &&\
+curl -L https://raw.githubusercontent.com/yezune/home/main/.tmux.conf > ~/.tmux.conf
